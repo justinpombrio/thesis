50501a16c397930d9f35f30c4688fd0cc1f52ece
Justin <justinpombrio@cs.brown.edu>
2018-02-23 14:46:06 -0500

    Finish and clean up C++ sugar overview

--- sugars/c++/eval_order.cpp
+++ sugars/c++/eval_order.cpp
@@ -19,14 +19,17 @@ void second() {
 
 template<typename B>
 int outer(int arg) {
-  static_assert(sizeof(B*) == 7, "outer");
-  return 0;
+  static_assert(sizeof(B*) == 7, "Begin outer call");
+  int answer = 0;
+  static_assert(sizeof(B*) == 7, "End outer call");
+  return answer;
 }
 
 template<typename A>
 int inner() {
-  static_assert(sizeof(A*) == 7, "inner");
+  static_assert(sizeof(A*) == 7, "Begin inner call");
   return 0;
+  static_assert(sizeof(A*) == 7, "End inner call");
 }
 
 // And the order of the errors suggests that
--- sugars/c++/run_eval_order2.sh
+++
@@ -1,1 +0,0 @@
-g++ eval_order2.cpp -o eval_order2 -std=c++11 && ./eval_order2
