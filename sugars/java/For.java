9eddc1ab1d34bf72414eff40be883c272c1aa47a
Justin <zallambo@gmail.com>
2018-03-08 13:20:51 -0500

    A familiar sugar

---
+++ sugars/java/For.java
@@ -0,0 +1,8 @@
+public class For {
+  public static void main(String[] args) {
+    int[] best_numbers = {1, 1, 2, 5, 14};
+    for (int n : best_numbers) {
+      System.out.println(n);
+    }
+  }
+}
