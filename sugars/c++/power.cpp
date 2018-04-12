525d3a7fef2dc2ecc4425e52c1ec5e4ec48a4b1d
Justin <justinpombrio@cs.brown.edu>
2018-04-12 16:51:13 -0400

    C++: template-defining templates

--- sugars/c++/power.cpp
+++ sugars/c++/power.cpp
@@ -2,14 +2,22 @@
 
 using namespace std;
 
+template<class A>
+struct Foo {
+  template<class B> struct Bar {
+    A fieldA;
+    B fieldB;
+  };
+};
 
-template<typename A>
-//template<typename B> syntax error: templates cannot define templates
-int add(A a, A b) {
-  return a + b;
-}
 
 int main() {
-  std::cout << add<int>(1, 2) << std::endl;
+  using IntFoo = Foo<int>;
+  using IntFooFloatBar = IntFoo::Bar<float>;
+  using IntFooCharBar = IntFoo::Bar<char>;
+  IntFooFloatBar bar1 = {6,6.6};
+  IntFooCharBar bar2 = {6, '6'};
+  std::cout << bar1.fieldA + bar1.fieldB << std::endl;
+  std::cout << bar2.fieldA + bar2.fieldB << std::endl;
   return 0;
 }
